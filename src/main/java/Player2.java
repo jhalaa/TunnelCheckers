9ef11e56102d8ed78c1b0655805516e0337c73ab
Jhalaa <jhalaachinoy@gmail.com>
2018-04-12 15:27:05 -0700

    Adding player2

--- src/main/java/Player2.java
+++ src/main/java/Player2.java
@@ -1,8 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Player2 implements Player {
     static final int PLAYER_STARTING_ROW = 5;
@@ -10,14 +6,14 @@ public class Player2 implements Player {
     private Set<Piece> checkers;
     private Set<Piece> kings;
 
-    Player2(){
+    Player2() {
         this.checkers.addAll(getPlayerPieces());
         this.kings = new HashSet<>();
     }
 
     private Set<Piece> getPlayerPieces() {
         Set<Piece> players = new HashSet<>();
-        for (int i = PLAYER_STARTING_ROW; i < PLAYER_STARTING_ROW+3; i++) {
+        for (int i = PLAYER_STARTING_ROW; i < PLAYER_STARTING_ROW + 3; i++) {
             int j = i % 2 == 0 ? 1 : 0;
             while (j < NUMBER_OF_COLUMNS) {
                 Piece piece = new Piece(i, j, false);
@@ -41,11 +37,63 @@ public class Player2 implements Player {
         return "Player2";
     }
 
-    public boolean move() {
+    public boolean move(Set<Piece> oppsitionCheckers) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the row number of piece you want to move");
+        int row = s.nextInt();
+        System.out.println("Enter the column number of piece you want to move");
+        int column = s.nextInt();
 
-        return false;
+        System.out.println("Enter the new row number of piece you want to move");
+        int newRow = s.nextInt();
+
+        System.out.println("Enter the new column number of piece you want to move");
+        int newColumn = s.nextInt();
+
+        if (isValidMove(row, column, newRow, newColumn, oppsitionCheckers)) {
+            Iterator<Piece> iterator = checkers.iterator();
+            while (iterator.hasNext()) {
+                Piece peice = iterator.next();
+                if (peice.getRow() == row && peice.getColumn() == column) {
+                    checkers.remove(peice);
+                    checkers.add(new Piece(newRow, newColumn, peice.isKing()));
+                    break;
+                }
+            }
+        }
+        return true;
     }
 
+    private boolean isValidMove(int row, int column, int newRow, int newColumn, Set<Piece> oppsitionCheckers) {
 
+        return
+                // there is no pawn diagonally opposite and the checker can be moved
+                newRow == row - 1 && (newColumn == column + 1 || newColumn == column - 1) && noPlayerinCell(newRow, newColumn) && noOppositionInCell(newRow, newColumn, oppsitionCheckers);
+
+    }
+
+    private boolean noOppositionInCell(int newRow, int newColumn, Set<Piece> oppsitionCheckers) {
+        Iterator<Piece> iterator = oppsitionCheckers.iterator();
+        while (iterator.hasNext()) {
+            Piece peice = iterator.next();
+            if (peice.getRow() == newRow && peice.getColumn() == newColumn) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean noPlayerinCell(int newRow, int newColumn) {
+        Iterator<Piece> iterator = checkers.iterator();
+        while (iterator.hasNext()) {
+            Piece peice = iterator.next();
+            if (peice.getRow() == newRow && peice.getColumn() == newColumn) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
+
+
